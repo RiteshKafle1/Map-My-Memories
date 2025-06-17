0c07a8db393fb373978bec13c74d427df1e9c34d
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-06-17 20:34:11 +0545

    multer configuration done.

---
+++ Backend/Config/multer.config.js
@@ -0,0 +1,9 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+    cb(null, `${file.filename - Date.now()}`);
+  },
+});
+const upload = multer({ storage });
+module.exports = upload;
