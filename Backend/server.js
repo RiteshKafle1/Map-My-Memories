1a20c1d29f43b39e57cddcaac3c385bfa2b11437
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-06-21 14:03:35 +0545

    adding a middleware to handle form data.

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
+const connectDB=require('../Backend/DB/connect.db');
+const userRouter = require("./Routes/user.routes");
+const travelRouter = require("./Routes/travelStory.routes");
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use('/user',userRouter);
+app.use('/travel',travelRouter);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server Running :) "));
+app.listen(PORT, () => {
+  connectDB(process.env.DB_URI);
+  console.log("Server Running :) ")
+});
