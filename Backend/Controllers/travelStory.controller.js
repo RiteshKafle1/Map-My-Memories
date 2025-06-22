695ee12170ca3c611934e4dcd6823c1fc53fd39e
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-06-22 13:28:11 +0545

    work done for function to update the travel story

--- Backend/Controllers/travelStory.controller.js
+++ Backend/Controllers/travelStory.controller.js
@@ -1,6 +1,5 @@
 const travelModel = require("../Models/travel.model");
 const cloudinary = require("../Config/cloud.config");
-const userModel = require("../Models/user.model");
 
 const addTravelStory = async (req, res) => {
   try {
@@ -60,4 +59,49 @@ const userTravelStory = async (req, res) => {
     res.json({ error: true, message: "Failed to fetch the user story" });
   }
 };
-module.exports = { addTravelStory, userTravelStory };
+
+const editTravelStory = async (req, res) => {
+  try {
+    const { title, story, visitedLocation, visitedDate, isFavourite } =
+      req.body;
+    const storyId = req.params.storyId;
+
+    const imageFile = req.file;
+
+    let imageUrl = "";
+
+    if (imageFile) {
+      const image = await cloudinary.uploader.upload(imageFile.path, {
+        resource_type: "image",
+        folder: "Story-Image",
+      });
+      imageUrl = image.secure_url;
+    }
+    const TravelStory = await travelModel.findById(storyId);
+
+    // console.log(TravelStory);
+
+    if (!TravelStory)
+      return res.json({ error: true, message: "No story found" });
+
+    TravelStory.title = title || TravelStory.title;
+    TravelStory.story = story || TravelStory.story;
+    TravelStory.visitedLocation =
+      visitedLocation || TravelStory.visitedLocation;
+    TravelStory.visitedDate = visitedDate || TravelStory.visitedDate;
+    TravelStory.isFavourite = isFavourite || TravelStory.isFavourite;
+    TravelStory.image = imageUrl || TravelStory.image;
+
+    const updatedStory = await TravelStory.save();
+
+    return res.json({
+      error: false,
+      message: " Travel story Updated",
+      updatedStory,
+    });
+  } catch (error) {
+    console.log("Error in editing travel story", error);
+    return res.json({ error: true, message: "Failed to update your story" });
+  }
+};
+module.exports = { addTravelStory, userTravelStory, editTravelStory };
